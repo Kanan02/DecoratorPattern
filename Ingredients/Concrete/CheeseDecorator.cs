412850d0948072740971d02d7b82e44c1a64a637
Gurb_bx51@itstep.edu.az <Gurb_bx51@itstep.edu.az>
2021-09-28 21:42:16 +0400

    Add project files.

---
+++ Ingredients/Concrete/CheeseDecorator.cs
@@ -0,0 +1,23 @@
+﻿using DecoratorExample.Ingredients.Abstract;
+using DecoratorExample.Pizzas.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorExample.Ingredients.Concrete
+{
+    public class CheeseDecorator:Ingredient
+    {
+        public CheeseDecorator(Pizza pizza):base(pizza.Price+=1,pizza.Name+"with chesse",pizza)
+        {
+
+        }
+
+        public override double getPrice()
+        {
+            Console.WriteLine("Here is your pizza +" + Name);
+            Console.WriteLine("Price: " + Price);
+            return Price;
+        }
+    }
+}
